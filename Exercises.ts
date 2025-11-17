aed7b20fcf4f3af56fe83e49f2d21f207a342a38
Olivier-Masabo <masaboolivier21@gmail.com>
2025-11-17 20:31:51 +0200

    beginner typescript exercises

---
+++ Exercises.ts
@@ -0,0 +1,22 @@
+  // Beginner level exercises
+ //exercise 1
+let myName:string = "Masabo";
+let male:boolean = true;
+const age:number = 21;
+
+console.log(myName,male,age)
+
+//exercise 2
+function addNumbers(a:number,b:number){
+    return a + b
+}
+console.log(addNumbers(10,40));
+
+//exercise 3
+type stock= {id:number,name:string,price:number}
+const product: stock[] =[
+    {id:1,name:"t-shirt",price:5000},
+     {id:2,name:"trouser",price:10000},
+      {id:3,name:"hoodie",price:15000}
+]
+console.log(product)
